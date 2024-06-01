91de0af21c6311a8ce672252623744141adc48d5
mrgabdev <gabriel.escobar.torres@gmail.com>
2024-06-01 17:56:01 -0600

    refactor: 📱 add nextjs page root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import { SwitchTheme } from '@/molecules'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import '../index.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SwitchTheme />
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,9 @@
-'use client'
-
 import { Heading } from '@/atoms/heading/Heading'
-import '../index.scss'
-import { Button } from '@/atoms/Button/Button'
-import { PopUp } from '@/molecules/pop-up/PopUp'
-import { useState } from 'react'
 
-// TODO: Remove components
 export default function Home() {
-  const [open, setOpen] = useState(false)
-
-  const hello = () => {
-    console.log('Enviando datos al servidor')
-    setOpen(false)
-  }
-
-  const openModal = () => {
-    setOpen((prev) => !prev)
-  }
-
   return (
-    <main>
-      {/* <Heading tag='h1'>some text</Heading> */}
-      <Button action={openModal}>Enviar datos</Button>
-      {open ? (
-        <PopUp
-          title='Modificar datos'
-          message='Se van a modificar los datos, estas seguro?'
-          button={<Button action={hello}>modificar</Button>}
-        />
-      ) : null}
-    </main>
+    <>
+      <Heading tag='h1'>Section</Heading>
+    </>
   )
 }
